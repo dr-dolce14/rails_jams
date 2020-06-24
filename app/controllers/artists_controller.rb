1737c9e447500d1d0170df42ce0c825fbe5832c7
Bearinawolfpack <bearinawolfpack@gmail.com>
2020-06-24 16:35:46 -0400

    CRUDdy Buddy Artist App

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -16,4 +16,29 @@ class ArtistsController < ApplicationController
     redirect_to artist_path(@artist)
   end
 
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+   
+  def update
+    @artist = Artist.find(params[:id])
+    @artist.update(name: params[:artist][:name], age: params[:artist][:age], bio: params[:artist][:bio])
+    redirect_to artist_path(@artist)
+  end
+
+  # def delete
+  #   @artist = Artist.find(params[:id])
+  # end
+
+  def destroy
+    @artist = Artist.find(params[:id])
+    @artist.destroy
+    redirect_to artists_path
+  end
+
+  # def destroy
+  #   @artist.destroy
+  #   redirect_to artists_path
+  # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  get 'artists/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'artists/index'
+  # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/artists', to: 'artists#index'
-  get 'artists/new', to: 'artists#new', as: "new_artist"
+  get 'artists/new', to: 'artists#new', as: 'new_artist'
   get '/artists/:id', to: 'artists#show', as: 'artist'
   post '/artists', to: 'artists#create'
+  get '/artists/:id/edit', to: 'artists#edit', as: :edit_article
+  patch '/artists/:id', to: 'artists#update'
+  delete '/artists/:id', to: 'artists#destroy', as: :delete_artist
+  # resources :artists
 end
